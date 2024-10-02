d416ac92953bb7ba1aa29b02ff1c63cbe0f966ce
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-10-02 21:59:42 +0000

    created associations for photo class

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -15,19 +15,23 @@
 class Photo < ApplicationRecord
   validates(:poster, { :presence => true })
 
-  # Association accessor methods to define:
-  
-  ## Direct associations
+  ## DIRECT ASSOCIATIONS
 
   # Photo#poster: returns a row from the users table associated to this photo by the owner_id column
+  belongs_to(:poster, class_name:"User", foreign_key:"owner_id")
 
   # Photo#comments: returns rows from the comments table associated to this photo by the photo_id column
+  has_many(:comments, class_name:"Comment", foreign_key:"photo_id")
 
   # Photo#likes: returns rows from the likes table associated to this photo by the photo_id column
-
-  ## Indirect associations
+  has_many(:likes, class_name:"Like", foreign_key:"photo_id")
+  ###################################################################################################################
+  ## INDIRECT ASSOCIATIONS
 
   # Photo#fans: returns rows from the users table associated to this photo through its likes
+  has_many(:fans, through: :likes, source:"fan_id")
+
+  ###################################################################################################################
 
   def poster
     my_owner_id = self.owner_id
