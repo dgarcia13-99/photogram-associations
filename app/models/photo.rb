a97b58cb2858d734afeca49e87c0c3a561494c50
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-10-03 02:26:58 +0000

    added associations to 'like' model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -29,49 +29,9 @@ class Photo < ApplicationRecord
   ## INDIRECT ASSOCIATIONS
 
   # Photo#fans: returns rows from the users table associated to this photo through its likes
-  has_many(:fans, through: :likes, source:"fan_id")
+  has_many(:fans, through: :likes, source: :fan)
 
   ###################################################################################################################
-
-  def poster
-    my_owner_id = self.owner_id
-
-    matching_users = User.where({ :id => my_owner_id })
-
-    the_user = matching_users.at(0)
-
-    return the_user
-  end
-
-  def comments
-    my_id = self.id
-
-    matching_comments = Comment.where({ :photo_id => self.id })
-
-    return matching_comments
-  end
-
-  def likes
-    my_id = self.id
-
-    matching_likes = Like.where({ :photo_id => self.id })
-
-    return matching_likes
-  end
-
-  def fans
-    my_likes = self.likes
-    
-    array_of_user_ids = Array.new
-
-    my_likes.each do |a_like|
-      array_of_user_ids.push(a_like.fan_id)
-    end
-
-    matching_users = User.where({ :id => array_of_user_ids })
-
-    return matching_users
-  end
 
   def fan_list
     my_fans = self.fans
@@ -86,4 +46,5 @@ class Photo < ApplicationRecord
 
     return formatted_usernames
   end
+  
 end
